1a798eec367f5bdae768b43960d77265a791648e
Moacir Silva Abreu <moacir.abreu.943@gmail.com>
2026-07-01 22:03:26 -0300

    Inserção do .env

--- src/config.py
+++ src/config.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DB_CONFIG = {
-    "host": "localhost",
-    "port": 5432,
-    "dbname": "monitoria",
-    "user": "postgres",
-    "password": "postgres"
+    "host": os.getenv("DB_HOST", "localhost"),
+    "port": os.getenv("DB_PORT", "5432"),
+    "dbname": os.getenv("DB_NAME", "monitoria"),
+    "user": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD", "postgres")
 }
